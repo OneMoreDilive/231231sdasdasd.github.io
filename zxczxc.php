0f92b8fe0b0a2c93c90c4c5eb1f0f589e53bcbbb
Nguyễn Bảo Toàn <91012643+OneMoreDilive@users.noreply.github.com>
2022-06-01 09:04:47 +0700

    Update zxczxc.php

--- zxczxc.php
+++ zxczxc.php
@@ -1,15 +1,1 @@
-{"clients":<?php
-							$file = file_get_contents('http://15.235.128.46:30120/dynamic.json');
-							$decode = json_decode($file, false);
-							$clients = $decode->clients;
-							$svmaxclients = $decode->sv_maxclients;
-							echo $clients;
-
-							?>,"gametype":"Freeroam","hostname":"^1[VIETNAM] ^2XĂ“M TUI ROLEPLAY | ^3FREE | ^4Thá»i Trang | ^5Giáº£i TrĂ­ | ^6Há»— Trá»£ Newbie","iv":"322656731","mapname":"fivem-map-hipster","sv_maxclients":"<?php
-							$file = file_get_contents('http://15.235.128.46:30120/dynamic.json');
-							$decode = json_decode($file, false);
-							$clients = $decode->clients;
-							$svmaxclients = $decode->sv_maxclients;
-							echo $svmaxclients;
-
-							?>"}
+{"clients":5,"gametype":"Freeroam","hostname":"^1[VIETNAM] ^2XĂ“M TUI ROLEPLAY | ^3FREE | ^4Thá»i Trang | ^5Giáº£i TrĂ­ | ^6Há»— Trá»£ Newbie","iv":"322656731","mapname":"fivem-map-hipster","sv_maxclients":"5"}
